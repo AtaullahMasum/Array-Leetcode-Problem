f2725a43733cc3786d5d480e7b697d620a16f1a0
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-21 23:27:33 +0600

    using natural number formula

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -12,4 +12,12 @@ class Solution:
             if nums[i] != i:
                 return i
         return n
+#Using natural number formula
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        total_sum = n*(n+1)//2 
+        nums_sum = sum(nums)
+        return total_sum - nums_sum
+
         
