294cc691040a8b1e225e6767e8a503b5430f92c5
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-21 23:24:25 +0600

    using Cyclic Sort Solution Added

---
+++ 268. Missing Number.py
@@ -0,0 +1,15 @@
+# Using Cyclic Sort
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n, i = len(nums) , 0
+        while i < n:
+            correct_index = nums[i]
+            if nums[i] < n and nums[i] != nums[correct_index]:
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+            else:
+                i += 1
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        return n
+        
