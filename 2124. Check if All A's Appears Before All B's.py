a837d1bf74c3e2fcf9effe0b0bfd3560c234f7de
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-24 00:37:27 +0600

    check if all A appear before B

---
+++ 2124. Check if All A's Appears Before All B's.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        for i in range(len(s)-1):
+            if ord(s[i]) > ord(s[i+1]):
+                return False
+        return True
+
+        
