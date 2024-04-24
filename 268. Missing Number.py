02c934f30000b50c8a86238dae54efe048639631
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-24 16:21:04 +0600

    some comment updated

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -19,7 +19,7 @@ class Solution:
         total_sum = n*(n+1)//2 
         nums_sum = sum(nums)
         return total_sum - nums_sum
-# Using Bitwise Exor 
+# Using Bitwise xor 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
        xor1 = 0
