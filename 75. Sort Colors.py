1b93a7265e312c5f02dd44115140ec4ca8107015
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-25 00:15:46 +0600

    better solution added

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -5,4 +5,26 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         nums.sort()
-        
+# Better Solution
+#  Time Complexity is O(n) + O(n)
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        cnt0 = 0
+        cnt1 = 0
+        cnt2 = 0
+        for num in  nums:
+            if num == 0:
+                cnt0 += 1
+            elif num == 1:
+                cnt1 += 1
+            else:
+                cnt2 += 1
+        for i in range(cnt0):
+            nums[i] = 0
+        for i in range(cnt1):
+            nums[cnt0+i] = 1
+        for i in range(cnt2):
+            nums[cnt0+cnt1+i] = 2
