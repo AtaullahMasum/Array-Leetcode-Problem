846a63a339a5c87e5689e8978f879d956fcafc55
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-24 13:59:30 +0600

    optimal solution added

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -15,3 +15,19 @@ class Solution:
         remain = len(temp)
         for i in range(remain, len(nums)):
             nums[i] = 0
+# Optimal Solution
+# Time Complexity is O(n)
+# Space Complexity is O(1)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right = 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+            
