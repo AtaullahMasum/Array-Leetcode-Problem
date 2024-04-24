4c81e1d4301d3ca2cb3433824f19b57a2015ef14
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-24 23:40:16 +0600

    optimal solution added

---
+++ 167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,12 @@
+# Optimal Solution using 2 pointer 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left , right = 0,len(nums)-1
+        while left < right:
+            if nums[left] + nums[right] == target:
+                return [left+1, right+1]
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            elif nums[left] + nums[right] < target:
+                left += 1
+        return [-1,-1]
