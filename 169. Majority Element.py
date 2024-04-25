518a5000ac98a7cea0f1578a3768f8e7fe0a0990
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-25 14:27:34 +0600

    Optimal Solution added

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -35,6 +35,29 @@ class Solution:
         for key, value in hashMap.items():
             if value > n//2:
                 return key
-# Optimal Solution 
+# Optimal Solution Using Moore's Voting Algorithm
 # Time Complexity is O(n)
 # Space Complexcity is O(1)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt = 1
+        element = nums[0]
+        for i in range(1,len(nums)):
+            if cnt == 0:
+                cnt = 1
+                element = nums[i]
+            elif nums[i] == element:
+                cnt += 1
+            else:
+                cnt -= 1
+        return element
+        # check for element is majority or not
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] ==element:
+                count += 1
+        if count > len(nums)//2:
+            return element
+        else:
+            return -1
+
