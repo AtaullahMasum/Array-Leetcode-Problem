eb3e51392ed179552978307f82dd2fe49fc9a84f
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-29 22:25:04 +0600

    better solution added

--- 18. 4Sum.py
+++ 18. 4Sum.py
@@ -17,3 +17,25 @@ class Solution:
                                 result.append(temp)
                                 hashSet.add(temp_tuple)
         return result
+# Better Solution using Hashing
+# Time Complexity: O(n^3)
+# space complexity : O(2* no. of quadruplets)
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        Set = set()
+        result = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                hashSet = set()
+                for k in range(j+1, len(nums)):
+                   remain = target - (nums[i] + nums[j] + nums[k])
+                   if remain in hashSet:
+                      temp = [nums[i], nums[j], nums[k], remain]
+                      temp.sort()
+                      temp_tuple = tuple(temp)
+                      if temp_tuple not in Set:
+                         result.append(temp)
+                         Set.add(temp_tuple)
+                   hashSet.add(nums[k])
+
+        return result
