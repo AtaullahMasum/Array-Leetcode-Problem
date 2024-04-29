7071c408765f29bb09e9ed9277ef2a7454419de0
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-29 23:40:45 +0600

    optimal solution added

--- 18. 4Sum.py
+++ 18. 4Sum.py
@@ -39,3 +39,34 @@ class Solution:
                    hashSet.add(nums[k])
 
         return result
+# Optimal solution using sorting + two pointer 
+# Time Complexity: O(n^2 + nlogn)
+# space complexity : O( no. of quadruplets)
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        nums.sort()
+        n = len(nums)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, n):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                left , right = j + 1, n - 1
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    if total  < target:
+                        left += 1
+                    elif total > target:
+                        right -= 1
+                    elif total == target:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right  and nums[left] == nums[left+1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+        return result
+
