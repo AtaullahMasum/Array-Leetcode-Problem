55f709d612053e340ea22ae5b638d8d6b50db13c
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-30 07:44:19 +0600

    all two solution added

---
+++ 56. Merge Intervals.py
@@ -0,0 +1,33 @@
+# Brute Force Solution 
+# Time Complexity is : O(nlogn + 2n)
+# Space Complexity is : O(1)
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = []
+        intervals.sort()
+        for i in range(len(intervals)):
+            start = intervals[i][0]
+            end = intervals[i][1]
+            if ans and end <= ans[-1][1]:
+                continue
+            for j in range(i+1, len(intervals)):
+                if intervals[j][0] <= end:
+                    end = max(end, intervals[j][1])
+                else:
+                    break
+            ans.append([start, end])
+        return ans 
+# Optimal Solution 
+# Time COmplexity is O(n)
+# Space Complexity is O(no. of unique intervals)
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = []
+        intervals.sort()
+        for i in range(len(intervals)):
+            if not ans or ans[-1][1] < intervals[i][0]:
+                ans.append(intervals[i])
+            else:
+                ans[-1][1] = max(ans[-1][1], intervals[i][-1])
+        return ans 
+        
