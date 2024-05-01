2274cb4bc87d24051c5f6b0766a8456d62158ce5
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-01 11:55:49 +0600

    reverse count pair optimal solution added

--- 493. Reverse Pairs.py
+++ 493. Reverse Pairs.py
@@ -1,6 +1,7 @@
 # Brute Force Solution using two loop
 # Time Complexity is O(n^2)
 # Space Complexity is O(1)
+from typing import List
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
         cnt = 0
@@ -9,4 +10,46 @@ class Solution:
                 if nums[i] > 2*nums[j]:
                     cnt += 1
         return cnt
-        
+# Optimal SOlution using Merge Sort
+# Time Complexity is O(2nlogn)
+# Space Complexity is O(n)
+class Solution:
+    def merge(self, nums, low, mid, high):
+        temp = []
+        left , right = low, mid+1 
+        while left <= mid and right <= high:
+            if nums[left] <=  nums[right]:
+                temp.append(nums[left])
+                left += 1
+            else:
+                temp.append(nums[right])
+                right += 1
+        while left <= mid:
+            temp.append(nums[left])
+            left += 1
+        while right <= high:
+            temp.append(nums[right])
+            right += 1
+        for i in range(low , high+1):
+            nums[i] = temp[i-low]
+    def countPairs(self, nums, low, mid, high):
+        cnt = 0
+        right = mid + 1
+        for i in range(low , mid+1):
+            while right <= high and nums[i] > 2*nums[right]:
+                right += 1
+            cnt += (right - (mid+1))
+        return cnt
+    def mergeSort(self, nums, low, high):
+        cnt = 0
+        if low >= high:
+            return  cnt
+        mid = (low+high)//2
+        cnt += self.mergeSort(nums, low, mid)
+        cnt += self.mergeSort(nums, mid+1, high)
+        cnt += self.countPairs(nums, low, mid, high)
+        self.merge(nums, low, mid, high)
+        return cnt
+
+    def reversePairs(self, nums: List[int]) -> int:
+        return self.mergeSort(nums, 0, len(nums)-1)       
