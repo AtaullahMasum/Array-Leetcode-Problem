2a2d2edd53b65969f83f6060da6edbc9818f9543
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-01 14:58:50 +0600

    optimal solution added

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -30,3 +30,22 @@ class Solution:
                 if product > maxi:
                     maxi = product             
         return maxi
+# Optimal Solution using prefix and sufix
+# Time Complexity is O(n)
+# Space Complexity is O(1)
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+       maxi = float('-inf')
+       prefix = 1
+       sufix = 1
+       n = len(nums)
+       for i in range(n):
+          if prefix == 0:
+             prefix = 1
+          if sufix == 0:
+             sufix = 1
+          prefix *= nums[i]
+          sufix *= nums[n-i-1]
+          maxi = max(maxi, prefix, sufix)
+          
+       return maxi
